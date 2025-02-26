408acdd271c202b70e0bbb05402b16cad9a41c9d
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-02-27 01:21:46 +0600

    Update MainActivity.java

--- Lessons/BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ Lessons/BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Wow, Yo Yo", Toast.LENGTH_LONG).show();
 
     }
 }
