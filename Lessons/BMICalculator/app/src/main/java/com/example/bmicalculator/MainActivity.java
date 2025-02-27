100843b471401d96c56870c7f4db3c9ffe066bf2
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-02-28 01:31:42 +0600

    program

--- Lessons/BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ Lessons/BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -12,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Wow, Yo Yo", Toast.LENGTH_LONG).show();
+        // Find the views by their IDs
+        RadioButton radioButtonMale = findViewById(R.id.radio_button_male);
+        RadioButton radioButtonFemale = findViewById(R.id.radio_button_female);
+
+        EditText editTextAge = findViewById(R.id.edit_text_age);
+        EditText editTextWeight = findViewById(R.id.edit_text_weight);
+        EditText editTextHeight = findViewById(R.id.edit_text_feet);
+        EditText editTextHeightInches = findViewById(R.id.edit_text_inches);
+
+        Button buttonCalculate = findViewById(R.id.button_calculate);
+
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
 
     }
 }
