be8d139e4e764a3b7f17fddfc106e6083121fb7f
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-05-21 23:23:33 +0600

    Dependency added
    //CardView
        implementation(libs.cardview)

--- Projects/The HungryDeveloper/app/build.gradle.kts
+++ Projects/The HungryDeveloper/app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //CardView
+    implementation(libs.cardview)
 }
