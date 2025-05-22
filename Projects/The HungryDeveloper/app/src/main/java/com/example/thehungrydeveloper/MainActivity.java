6748c6d475d05b2084b8c84079b4a960c4a22745
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-05-22 23:55:11 +0600

    added the variables in the mainactivity

--- Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -4,16 +4,24 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView startersCard;
+    CardView mainCourseCard;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startersCard = findViewById(R.id.cardview_starters);
+        mainCourseCard = findViewById(R.id.cardview_main_course);
 
     }
 }
