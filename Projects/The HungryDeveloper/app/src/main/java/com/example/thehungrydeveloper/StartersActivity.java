a05df65b18e40a77f24e11edd83f446218dad20e
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-05-25 23:54:24 +0600

    Update StartersActivity.java

--- Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/StartersActivity.java
+++ Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/StartersActivity.java
@@ -19,10 +19,11 @@ public class StartersActivity extends AppCompatActivity {
 
         ListView starterListView = findViewById(R.id.listView_starters);
 
-        Dish dish1 = new Dish();
-        dish1.title = "Spring Rolls";
-        dish1.description = "Crispy rolls filled with vegetables and served with sweet chili sauce.";
-        dish1.price = 5;
+        Dish dish1 = new Dish(
+                "Spring Rolls",
+                "Crispy rolls filled with vegetables and served with sweet chili sauce.",
+                5
+        );
 
         String[] dishes = {
             "Spring Rolls",
