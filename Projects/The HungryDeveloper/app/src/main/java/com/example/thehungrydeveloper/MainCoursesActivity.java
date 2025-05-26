5bf2b3bd48cc1a135cce4286027342e2a5c11951
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-05-26 22:57:15 +0600

    Updated in the Android Manifest for the MainCoursesActivity

--- Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainCoursesActivity.java
+++ Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainCoursesActivity.java
@@ -9,7 +9,7 @@ public class MainCoursesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView();
+        setContentView(R.layout.activity_main_courses);
 
     }
 }
