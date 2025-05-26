fc1c2f2d657d1b4b46aec9dbc6b7da252b896ac6
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-05-26 23:43:15 +0600

    Update MainActivity.java

--- Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ Projects/The HungryDeveloper/app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        
+
         mainCourseCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
