3b3c0ab195f2ccd285cf4b52ddcf78b421d53845
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-05-29 23:05:28 +0600

    added recyclerview in activity_main.xml

--- Projects/MyAppPortfolio/app/build.gradle.kts
+++ Projects/MyAppPortfolio/app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    
+
     // RecyclerView
     implementation(libs.recyclerview)
     //CardView
