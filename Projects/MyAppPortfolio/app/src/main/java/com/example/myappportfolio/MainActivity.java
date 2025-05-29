34a9b5b4c9594cbc46028c9f781733d270960696
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-05-29 23:52:10 +0600

    Update MainActivity.java
    updated the Project array with the new image added in the drawables

--- Projects/MyAppPortfolio/app/src/main/java/com/example/myappportfolio/MainActivity.java
+++ Projects/MyAppPortfolio/app/src/main/java/com/example/myappportfolio/MainActivity.java
@@ -21,12 +21,11 @@ public class MainActivity extends AppCompatActivity {
                 5);
 
         Project[] projects = {
-                new Project("Spotify Streamer", "A music streaming app that allows users to search for artists, albums, and tracks.", 5),
-                new Project("Super Duper Quiz", "A quiz app that tests your knowledge on various topics with multiple-choice questions.", 6),
-                new Project("Library App", "An app that helps users manage their book collections, including borrowing and returning books.", 7),
-                new Project("Build It Bigger", "An app that provides users with construction challenges and allows them to build virtual structures.", 8),
-                new Project("XYZ Reader", "An e-book reader app that supports various formats and provides a smooth reading experience.", 9),
-                new Project("Capstone Project", "A comprehensive project that showcases all the skills learned in the course.", 10)
+                new Project("Spotify Streamer", "A music streaming app that allows users to search for artists, albums, and tracks.", R.drawable.getting_started),
+                new Project("Super Duper Quiz", "A quiz app that tests your knowledge on various topics with multiple-choice questions.", R.drawable.quote),
+                new Project("Library App", "An app that helps users manage their book collections, including borrowing and returning books.", R.drawable.calculator),
+                new Project("Build It Bigger", "An app that provides users with construction challenges and allows them to build virtual structures.", R.drawable.tape),
+                new Project("Capstone Project", "A comprehensive project that showcases all the skills learned in the course.", R.drawable.hungry_developer)
         };
     }
 }
