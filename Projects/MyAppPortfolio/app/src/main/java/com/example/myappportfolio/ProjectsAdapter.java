c475d48482950fc26ac39b373632a9af98d78b0d
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-07 20:05:06 +0600

    Update ProjectsAdapter.java
    Added:
    
    @NonNull
        @Override
        public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
            return null;
        }
    
        @Override
        public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
    
        }
    
        @Override
        public int getItemCount() {
            return 0;
        }

--- Projects/MyAppPortfolio/app/src/main/java/com/example/myappportfolio/ProjectsAdapter.java
+++ Projects/MyAppPortfolio/app/src/main/java/com/example/myappportfolio/ProjectsAdapter.java
@@ -1,16 +1,29 @@
 package com.example.myappportfolio;
 
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProjectsAdapter extends RecyclerView.Adapter<> {
+public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsAdapter.ProjectViewHolder> {
 
 
+    @NonNull
+    @Override
+    public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
 
+    }
 
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     static class ProjectViewHolder extends RecyclerView.ViewHolder {
 
