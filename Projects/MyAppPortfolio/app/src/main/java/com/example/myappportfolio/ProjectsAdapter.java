21fbce09f599ac3da2afbabe23defed263afe4ba
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-07 20:02:16 +0600

    Update ProjectsAdapter.java

--- Projects/MyAppPortfolio/app/src/main/java/com/example/myappportfolio/ProjectsAdapter.java
+++ Projects/MyAppPortfolio/app/src/main/java/com/example/myappportfolio/ProjectsAdapter.java
@@ -1,5 +1,8 @@
 package com.example.myappportfolio;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ProjectsAdapter extends RecyclerView.Adapter<> {
@@ -9,5 +12,10 @@ public class ProjectsAdapter extends RecyclerView.Adapter<> {
 
 
 
-    class
+    static class ProjectViewHolder extends RecyclerView.ViewHolder {
+
+        public ProjectViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
