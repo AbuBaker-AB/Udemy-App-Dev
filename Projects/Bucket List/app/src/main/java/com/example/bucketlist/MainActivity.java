625385e5f30d9cb867b39916ac7016daceb29404
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-19 22:36:49 +0600

    Update MainActivity.java

--- Projects/Bucket List/app/src/main/java/com/example/bucketlist/MainActivity.java
+++ Projects/Bucket List/app/src/main/java/com/example/bucketlist/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.bucketlist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupClickListeners();
+
+    }
+
+    private void setupClickListeners() {
+        CardView thingsToDoCard = findViewById(R.id.card_view_things_to_do);
+        CardView placesToGoCard = findViewById(R.id.card_view_places_to_go);
+
+        thingsToDoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThingsToDoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        placesToGoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlacesToGoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
