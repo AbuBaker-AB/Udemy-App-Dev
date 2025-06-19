55bec6f46ca5e6daa1a10a05c82e5dd487f6172e
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-20 00:23:54 +0600

    Create BucketListEntryAdapter.java

---
+++ Projects/Bucket List/app/src/main/java/com/example/bucketlist/BucketListEntryAdapter.java
@@ -0,0 +1,16 @@
+package com.example.bucketlist;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BucketListEntryAdapter extends RecyclerView<> {
+
+    static class BucketListViewHolder extends RecyclerView.ViewHolder {
+        public BucketListViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+        // ViewHolder implementation
+    }
+}
