d6c830d3074aec96cc48d2cc8f6bb1ad5504ee9c
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-20 00:22:22 +0600

    Update PlacesToGoActivity.java

--- Projects/Bucket List/app/src/main/java/com/example/bucketlist/PlacesToGoActivity.java
+++ Projects/Bucket List/app/src/main/java/com/example/bucketlist/PlacesToGoActivity.java
@@ -14,6 +14,21 @@ public class PlacesToGoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_to_go);
+        setupList();
+    }
 
+    private void setupList() {
+        BucketListEntry[] placesToGo = {
+                new BucketListEntry("Climb Mt kilimanjaro", "Do it the difficult way!", R.drawable.kilimanjaro, 5),
+                new BucketListEntry("Visit the Grand Canyon", "A natural wonder of the world", R.drawable.northern_lights, 4.5f),
+                new BucketListEntry("Skydiving in Dubai", "Experience the thrill of freefall", R.drawable.skydive, 4.8f),
+                new BucketListEntry("Explore the Amazon Rainforest", "Adventure in the heart of nature", R.drawable.amazon, 4.7f),
+                new BucketListEntry("Scuba Diving in the Great Barrier Reef", "Discover underwater wonders", R.drawable.japan, 4.9f),
+                new BucketListEntry("Hike the Inca Trail to Machu Picchu", "A journey through history", R.drawable.road_trip, 4.6f),
+                new BucketListEntry("Safari in Serengeti", "Witness the great migration", R.drawable.iceland, 4.8f),
+                new BucketListEntry("Visit the Pyramids of Giza", "Explore ancient wonders", R.drawable.scubadive, 4.5f),
+                new BucketListEntry("Hot Air Balloon Ride in Cappadocia", "Soar over unique landscapes", R.drawable.vietnam, 4.7f),
+                new BucketListEntry("Explore the Northern Lights in Iceland", "A magical natural phenomenon", R.drawable.kerala, 4.9f)
+        };
     }
 }
