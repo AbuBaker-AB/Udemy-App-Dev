12b361815f06e2598d032958fc3c45b8b2ecad48
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-20 00:28:29 +0600

    Update BucketListEntryAdapter.java

--- Projects/Bucket List/app/src/main/java/com/example/bucketlist/BucketListEntryAdapter.java
+++ Projects/Bucket List/app/src/main/java/com/example/bucketlist/BucketListEntryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.bucketlist;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -17,17 +18,18 @@ public class BucketListEntryAdapter extends RecyclerView.Adapter<BucketListEntry
     @NonNull
     @Override
     public BucketListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_bucket_list_entry, parent, false);
+        return new BucketListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull BucketListViewHolder holder, int position) {
-
+        
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return entries.length;
     }
 
     static class BucketListViewHolder extends RecyclerView.ViewHolder {
