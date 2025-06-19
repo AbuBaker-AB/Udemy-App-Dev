b3444a214d7cdd1f78fe17f6f7f3cf87b4848ce0
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-20 00:37:48 +0600

    Update BucketListEntryAdapter.java

--- Projects/Bucket List/app/src/main/java/com/example/bucketlist/BucketListEntryAdapter.java
+++ Projects/Bucket List/app/src/main/java/com/example/bucketlist/BucketListEntryAdapter.java
@@ -3,6 +3,9 @@ package com.example.bucketlist;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,7 +27,7 @@ public class BucketListEntryAdapter extends RecyclerView.Adapter<BucketListEntry
 
     @Override
     public void onBindViewHolder(@NonNull BucketListViewHolder holder, int position) {
-        
+
     }
 
     @Override
@@ -33,9 +36,22 @@ public class BucketListEntryAdapter extends RecyclerView.Adapter<BucketListEntry
     }
 
     static class BucketListViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView image;
+        private TextView headerText;
+        private TextView descriptionText;
+        private RatingBar ratingBar;
+
         public BucketListViewHolder(@NonNull View itemView) {
             super(itemView);
+            image = itemView.findViewById(R.id.imageView_item_image);
+            headerText = itemView.findViewById(R.id.text_view_item_heading);
+            descriptionText = itemView.findViewById(R.id.text_view_item_description);
+            ratingBar = itemView.findViewById(R.id.rating_bar_item_rating);
         }
-        // ViewHolder implementation
+        public void bind(BucketListEntry entry, int position) {
+
+        }
+
     }
 }
