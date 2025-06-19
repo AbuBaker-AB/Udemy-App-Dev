171c11a925f8a9f418afb07e4f73c12e9b70679f
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-06-20 00:19:40 +0600

    activity

--- Projects/Bucket List/app/src/main/java/com/example/bucketlist/BucketListEntry.java
+++ Projects/Bucket List/app/src/main/java/com/example/bucketlist/BucketListEntry.java
@@ -1,10 +1,24 @@
 package com.example.bucketlist;
 
+import androidx.annotation.DrawableRes;
+
 public class BucketListEntry {
 
     String heading;
     String description;
     int image;
-    float decimalValue;
-    
+    float rating;
+
+    public BucketListEntry(String heading, String description, @DrawableRes int image, float rating) {
+        this.heading = heading;
+        this.description = description;
+        this.image = image;
+        this.rating = rating;
+    }
+
+    /* @DrawableRes - This annotation indicates that the integer parameter is a reference to a drawable resource.
+        and the user cannot pass an arbitrary integer value, but must use a valid drawable resource ID defined in the app's resources.
+        This helps catch errors at compile time if an invalid resource ID is used. */
+
+
 }
