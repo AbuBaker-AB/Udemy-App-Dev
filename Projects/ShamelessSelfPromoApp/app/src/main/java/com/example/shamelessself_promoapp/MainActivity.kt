b3cfd52ace12311ac6ecc5b439c3cc722376112b
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-12-05 01:13:04 +0600

    Add preview button click handler to MainActivity
    Introduced a click listener for the preview button in MainActivity. On click, the app retrieves input from several text fields and displays a Toast with the contact name and number.

--- Projects/ShamelessSelfPromoApp/app/src/main/java/com/example/shamelessself_promoapp/MainActivity.kt
+++ Projects/ShamelessSelfPromoApp/app/src/main/java/com/example/shamelessself_promoapp/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.shamelessself_promoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val previewButton: Button = findViewById(R.id.previewButton)
+        previewButton.setOnClickListener {
+            // Handle preview button click
+            onPreviewClicked()
+
+        }
+
+    }
+
+    private fun onPreviewClicked() {
+        val contactNameEditText: TextInputEditText = findViewById(R.id.contactNameEt)
+        val contactNumberEditText: TextInputEditText = findViewById(R.id.contactNumber)
+        val displayNameEditText: TextInputEditText = findViewById(R.id.myDisplayName)
+        val messageEditText: TextInputEditText = findViewById(R.id.messageEt)
+
+        val testString =contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString()
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
