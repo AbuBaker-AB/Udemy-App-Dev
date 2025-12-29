9a800d039b3e706ad0fad19d60c56eade6ccbc1c
AbuBaker-AB <145045004+AbuBaker-AB@users.noreply.github.com>
2025-12-29 23:26:01 +0600

    Remove unused view references from MainActivity
    Cleaned up MainActivity by removing unused EditText, CheckBox, and Spinner variable declarations and their initializations. This simplifies the code and removes unnecessary references.

--- Projects/ShamelessSelfPromoApp/app/src/main/java/com/example/shamelessself_promoapp/MainActivity.kt
+++ Projects/ShamelessSelfPromoApp/app/src/main/java/com/example/shamelessself_promoapp/MainActivity.kt
@@ -15,26 +15,13 @@ import com.example.shamelessself_promoapp.R
 
 class MainActivity : AppCompatActivity() {
 
-    private var contactNameEditText: TextInputEditText? = null
-    private var contactNumberEditText: TextInputEditText? = null
-    private var displayNameEditText: TextInputEditText? = null
-    private var messageEditText: TextInputEditText? = null
-    private var juniorCheckBox: CheckBox? = null
-    private var immediateCheckBox: CheckBox? = null
-    private var groupSpinner: Spinner? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        contactNameEditText = findViewById(R.id.contactNameEt)
-//        contactNumberEditText = findViewById(R.id.contactNumber)
-        displayNameEditText = findViewById(R.id.myDisplayName)
-        messageEditText = findViewById(R.id.messageEt)
-        juniorCheckBox = findViewById(R.id.juniorCheckBox)
-        immediateCheckBox = findViewById(R.id.immediateStartCheckBox)
-        groupSpinner = findViewById(R.id.groupSpinner)
 
-        val previewButton: Button = findViewById(R.id.previewButton)
+
+        
         previewButton.setOnClickListener {
             // Handle preview button click
             onPreviewClicked()
